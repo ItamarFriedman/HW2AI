7bcb678c5b5c401e9e5d10764ac83f48eb7059d1
ItamarFriedman <itamarfr8@gmail.com>
2024-03-19 21:48:20 +0200

    more fixes and tuning

--- submission.py
+++ submission.py
@@ -10,40 +10,34 @@ def smart_heuristic(env: WarehouseEnv, robot_id: int):
     # distance form the closest package if robot has no package
     pickUpIncentive = (robot.package is None) * min(manhattan_distance(robot.position, env.packages[0].position),
                                                     manhattan_distance(robot.position, env.packages[1].position))
-    # finding the closest package
+    # finding the closest charge
     closestCharge = env.charge_stations[1]
     if manhattan_distance(robot.position, env.charge_stations[0].position) <= \
             manhattan_distance(robot.position, env.charge_stations[1].position):
         closestCharge = env.charge_stations[0]
-
-    # the minimum distance from the nearest charge to a package
-    extra = min(manhattan_distance(closestCharge.position, env.packages[0].position),
-                manhattan_distance(closestCharge.position, env.packages[1].position))
 
     closestPackageToCharge = env.packages[1]
     if manhattan_distance(closestCharge.position, env.packages[0].position) <= \
             manhattan_distance(closestCharge.position, env.packages[1].position):
         closestPackageToCharge = env.packages[0]
 
-    after_charge = manhattan_distance(closestCharge.position,closestPackageToCharge.position) + manhattan_distance(closestPackageToCharge.position, closestPackageToCharge.destination)
+    after_charge = manhattan_distance(closestCharge.position, closestPackageToCharge.position) + \
+                   manhattan_distance(closestPackageToCharge.position, closestPackageToCharge.destination)
 
     # if robot does have a package, setting his drop-off distance and extra distance from charge to drop-off
     dropOffIncentive = 0
     if robot.package is not None:
         dropOffIncentive = manhattan_distance(robot.position, robot.package.destination)
-        extra = manhattan_distance(closestCharge.position, robot.package.destination)
-        after_charge = extra
+        after_charge = manhattan_distance(closestCharge.position, robot.package.destination)
 
     # if battery is low
-    if robot.battery <= 1.5 * manhattan_distance(robot.position, closestCharge.position) and robot.battery > after_charge:
-
-        return 30000 - manhattan_distance(robot.position, closestCharge.position) + 50*(robot.package is not None)
+    if robot.battery <= 1.5 * manhattan_distance(robot.position, closestCharge.position) and \
+            (after_charge >= robot.battery + robot.credit):
+        return -manhattan_distance(robot.position, closestCharge.position) + 50 * (robot.package is not None)
     else:  # if battery is high enough
-        return 50000 - (15 * pickUpIncentive + dropOffIncentive - (100 * robot.battery) * (extra >= robot.battery) -
-                        50*robot.credit) + 50*(robot.package is not None) + robot.position[1]
-
-
-
+        return -(15 * pickUpIncentive + dropOffIncentive - (100 * robot.battery) *
+                 (after_charge >= robot.battery + robot.credit) -
+                 50 * robot.credit) + 50 * (robot.package is not None) + robot.position[1]
 
 
 class AgentGreedyImproved(AgentGreedy):
@@ -56,11 +50,13 @@ class AgentMinimax(Agent):
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
         start = time.time()
         depth = 0
-        o = 0
+        o = 'park'
         try:
             while True:
                 depth += 1
                 h, o = self.rb_minimax(env, agent_id, depth, True, start, time_limit)
+                if o == -1:
+                    return 'park'
         except TimeoutError:
             return o
 
@@ -101,17 +97,29 @@ class AgentMinimax(Agent):
     def heuristic(self, env: WarehouseEnv, robot_id: int):
         robot = env.get_robot(robot_id)
         other_robot = env.get_robot((robot_id + 1) % 2)
-        packageBonus = 10 * (robot.package is not None)
-        creditBonus = 100 * robot.credit
-        lowBatteryPanelty = 5000 * (robot.battery <= 0)
-        creditDifference = 1000 * (robot.credit - other_robot.credit)
-        return packageBonus + creditBonus + creditDifference - lowBatteryPanelty
+
+        if (env.done()):
+            return 10e6 * (robot.credit > other_robot.credit) - 10e6 * (robot.credit < other_robot.credit)
+        return smart_heuristic(env, robot_id) - smart_heuristic(env, not robot_id)
+
+        packageBonus = 1 * (robot.package is not None)
+        creditBonus = 10 * robot.credit
+        lowBatteryPanelty = 500 * (robot.battery <= 0)
+        creditDifference = 100 * (robot.credit - other_robot.credit)
+        ourHeuristic = packageBonus + creditBonus + creditDifference - lowBatteryPanelty
+
+        packageBonus = 1 * (other_robot.package is not None)
+        creditBonus = 10 * other_robot.credit
+        lowBatteryPanelty = 500 * (other_robot.battery <= 0)
+        creditDifference = 100 * (other_robot.credit - robot.credit)
+        otherHeuristic = packageBonus + creditBonus + creditDifference - lowBatteryPanelty
+        return ourHeuristic - otherHeuristic
 
 
 class AgentAlphaBeta(AgentMinimax):
     # TODO: section c : 1
     def rb_minimax(self, env, agent_id, depth, our_turn, start, time_limit, alpha=-float('inf'), beta=float('inf')) -> (
-    int, int):
+            int, int):
         currentTime = time.time()
         if currentTime - start >= 0.9 * time_limit:
             raise TimeoutError
